0ee7a449e1dae3e9a3fd53470b0dc0d3f9b8615c
Benjamin Salamon <benjamin.salamon@smail.th-koeln.de>
2020-12-06 21:52:55 +0100

    Aufgabe 1 fertiggestellt.

--- Uebungsaufgaben/Benjamin/grundlagen_javascript.js
+++ Uebungsaufgaben/Benjamin/grundlagen_javascript.js
@@ -1,12 +1,14 @@
-// Aufgabe 1 Namen auf der Konsole ausgeben
+/*
+  Aufgabenblatt 1 fertiggestellt von Benjamin Salamon am 06.12.2020
+ */
+
+// Aufgabe 1 Den eigenen Namen auf der Konsole ausgeben
 console.log("Benjamin Salamon");
 
-// Aufgabe 2 
-
-//  Initialisierung der Variablen und Ausgabe
+// Aufgabe 2 Deklarieren der Variablen und Konstanten
 const maxSterne = 5;
 let aktuelleAnzahlBewertungen = 0;
-let bewertung;
+let bewertung=0;
 
 //Initialisieren des readLine
 const readline = require('readline');
@@ -15,7 +17,9 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-//Abfrage nach Sternpunktzahl und Fehlerausgabe
+// Aufgabe 3 Nutzer soll in der Kommandozeile bewerten können. Falsche Eingaben sollen abgefangen werden.
+//manuelleBewertung()
+function manuelleBewertung(){
 rl.question(`Welche Bewertung würden Sie der App geben? (0 - schlechteste Bewertung bis ${maxSterne} Sterne): `, (answer) => {
   answer = Number(answer)
   if (answer > maxSterne || answer < 0) {
@@ -24,10 +28,34 @@ rl.question(`Welche Bewertung würden Sie der App geben? (0 - schlechteste Bewer
     console.log(new Error("Eingegebener Wert nicht vom Datentyp Number"));
   } else if (typeof answer === "number" && answer >= 0 && answer <= maxSterne) {
     aktuelleAnzahlBewertungen++;
-    bewertung = (bewertung + answer) / aktuelleAnzahlBewertungen;
+    bewertung = berechnungDerBewertung(answer,bewertung,aktuelleAnzahlBewertungen);
     console.log(`Ihre Bewertung von ${answer} Sternen wurde gespeichert.`);
   } else {
     console.log(new Error("Unbekannter Fehler."));
   }
-  rl.close();
-})
+  rl.close()
+});
+}
+
+//Aufgabe 4 Bewertung soll n-Mal berechnet werden. Jeder Berechnung wird eine neue zufällige Bewertung hinzugefügt.
+zufaelligeBewertung()
+function zufaelligeBewertung(){
+  rl.question('Wie oft soll eine zufällige Bewertung erzeugt werden? :',(answer) => {
+    answer = Number(answer)
+    let randomNumber;
+    for (i = 0; i<answer; i++){
+      aktuelleAnzahlBewertungen++;
+      randomNumber = Math.floor(Math.random() * (maxSterne+1))
+      bewertung = berechnungDerBewertung(randomNumber,bewertung,aktuelleAnzahlBewertungen);
+      console.log(`Abgegebene Bewertung: ${randomNumber}`)
+      console.log(`Durchschnitt Bewertung: ${bewertung}`)
+      console.log(`Anzahl der Bewertungen: ${aktuelleAnzahlBewertungen}\n`)
+    }
+    rl.close();
+  });
+}
+
+// Aufgabe 5 Berechnung der Bewertung in eine Funktion packen
+function berechnungDerBewertung (abgegebeneBewertung, aktuelleBewertung, aktuelleAnzahlBewertungen){
+  return ((((aktuelleAnzahlBewertungen-1)* aktuelleBewertung) + abgegebeneBewertung)/aktuelleAnzahlBewertungen);
+}
