a946745465081db6347b782e2aa206cfd27696eb
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2020-12-08 20:00:18 +0100

    node.js lernen

--- Uebungsaufgaben/Thomas/server_express.js
+++ Uebungsaufgaben/Thomas/server_express.js
@@ -7,8 +7,63 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+/* ROUTEN:
+* Funktionen für GET, POST, PUT, DELETE
+**
+* res.status()
+* Statuscodes bsp.:
+* 200 OK
+* 201 CREATED
+* 202 ACCEPTED
+* 204 NO CONTENT
+* 301 MOVED PERMANENTLY
+* 302 MOVED TEMPORARILY
+* 303 SEE OTHER
+* 400 BAD REQUEST
+* 401 UNAUTHORIZED
+* 403 FORBIDDEN
+* 404 NOT FOUND
+* 405 METHOD NOT ALLOWED
+* ...
+*/
+
 app.get('/', (req, res) => {
     res.send('Hallo Node.js! [mit express]');
+});
+
+app.get('/janedoe', (req, res) => {
+    const person = {
+        firstName: "Jane",
+        lastName: "Doe"
+    };
+    res.send(person);
+});
+
+app.get('/500', (req, res) => {
+    res.status('500').send('Interner Serverfehler').end();
+});
+
+// Optionale Angaben der URL können mit ? deklariert werden
+app.get('/date/:year/:month/:day?', (req, res) => {
+/*    
+    const   year = req.params.year,
+            month = req.params.month,
+            day = req.params.day || 1;
+*/
+    if (req.query.format === 'html') {
+        res.send(`<h1>${req.params.day}.${req.params.month}.${req.params.year}</h1>`);
+    } else { // oder return
+/* URL wird als String interpretiert, durch mathop lässt es sich in eine Zahl wandeln
+* [NUMBER] +- [STRING] = STRING
+* [STRING] +- [NUMBER] = NUMBER
+*/
+        res.send({
+            year: req.params.year - 0,
+            month: req.params.month - 0,
+            day: req.params.day - 0 || 0 //Wenn nicht angegeben dann || fallback 0
+        });
+    }
+
 });
 
 const server = http.createServer(app);
