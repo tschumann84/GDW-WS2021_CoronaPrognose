1e21281c6f42825583d703a2463a125cda7c3263
Nico-Gerasch <nico_justin.gerasch@smail.th-koeln.de>
2020-12-08 21:38:05 +0100

    Video 5

--- Uebungsaufgaben/Nico/Video 5/app.js
+++ Uebungsaufgaben/Nico/Video 5/app.js
@@ -3,17 +3,43 @@
 const http = require(`http`),
     path = require(`path`);
 
+const bodyParser = require(`body-parser`);
 const express = require(`express`);
 
 const app = express();
 
+
+//cors() ist eine Funtion die aufgerufen wird
+app.use(cors());
+
+app.use(bodyParser.json({
+    //limit: `100kb`,
+    //strict: false
+}));
+//Middelware ist sogesehen das hier
+app.use((req, res, next) => {
+   //...
+    next();
+});
+
+
 app.use(`/`, express.static(path.join(__dirname, `client`)));
 //app.use(`/`, express.static(path.join(__dirname, `someOtherClient`)));
+//Cookies dürfen nur by https mitgesendet werden
+app.get('/hello', (req, res) =>{
+  res.cookie(`user`, `jane.doe`, {
+      maxAge: 24 * 60 * 60 * 1000,
+      //secure: true
+    }).send(`Hallo World!`);
+});
 
 app.get(`/articles`,(req, res) =>{
     res.send([
         { id: 1, titel: `foo` }
         ]);
+});
+app.get(`/articles`,(req, res) =>{
+   res.send(`Hallo ${req.body.user}!`);
 });
 
 const server = http.createServer(app);
