d050e82ad2a9b875648370f9d45693e4d9051504
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2020-12-09 18:18:22 +0100

    proof_of_concept   today.js

---
+++ proof_of_concept/modules/today.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const today = function (filename) {
+    
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd;
+    }
+    if(mm<10) {
+        mm='0'+mm;
+    }
+    today = yyyy+'-'+mm+'-'+dd;
+    
+    return today+filename;
+        
+};
+
+module.exports = today;
+
+
+
