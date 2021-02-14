b6e860fa6eafc36f72458e8f3f0bf250ea6e4ccf
Benjamin Salamon <benjamin.salamon@smail.th-koeln.de>
2021-02-14 22:09:39 +0100

    Modul gebaut, welches die neuInfektionen in einem bestimmten Zeitraum berechnet.

--- proof_of_concept/modules/links.js
+++ proof_of_concept/modules/links.js
@@ -1,26 +1,38 @@
-// Test URI generierung
-
-const ersterUriPart = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?'
-
-const getURI = function(typ, link, datumVon, datumBis, suchbereich){
+const getNewZombies = function(typ, datumVon, datumBis, suchbefehl){
     // Typ 1 = Landkreis, Typ 2 = Bundesland, Typ3 Deutschlandweit
-    const letzterUriPart= '&outFields=*&f=json';
-    const datumFilter= `where=Meldedatum >= TIMESTAMP '${datumVon} 00:00:00' AND Meldedatum <= TIMESTAMP '${datumBis} 23:59:59'`
-    let filterKomplett;
+    const https = require('https');
+    const URI = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?'
+    const anwSUM=`&outStatistics=[{"statisticType": "sum","onStatisticField": "AnzahlFall","outStatisticFieldName": "summiertAnzahlFall"}]`;
+    const anwDiverse= '&outFields=*&f=json';
+    const anwFilter= `where=Meldedatum >= TIMESTAMP '${datumVon} 00:00:00' AND Meldedatum <= TIMESTAMP '${datumBis} 23:59:59'`
+    let filterZusammengesetzt;
 
     switch (typ){
         case 1:
-            filterKomplett = datumFilter + ` AND Bundesland = '${suchbereich}'`
+            filterZusammengesetzt = anwFilter + ` AND Landkreis = '${suchbefehl}'`
             break;
         case 2:
-            filterKomplett = datumFilter + ` AND Landkreis = '${suchbereich}'`
+            filterZusammengesetzt = anwFilter + ` AND Bundesland = '${suchbefehl}'`
             break;
         case 3:
-            filterKomplett = datumFilter
+            filterZusammengesetzt = anwFilter
             break;
     }
-    console.log(ersterUriPart+encodeURI(filterKomplett+letzterUriPart));
-}
+    const abfrageURI = URI+encodeURI(filterZusammengesetzt+anwSUM+anwDiverse);
+    https.get(abfrageURI,(res) => {
+        let body = "";
 
-//ersterTest
-getURI(1, ersterUriPart, '2021-01-13', '2021-01-14', 'Nordrhein-Westfalen');
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            let parsedData = JSON.parse(body);
+            //console.log(parsedData.features[0].attributes.summiertAnzahlFall)
+            console.log(parseInt(parsedData.features[0].attributes.summiertAnzahlFall));
+            return(parseInt(parsedData.features[0].attributes.summiertAnzahlFall));
+        });
+    });
+}
+getNewZombies(1,'2021-02-13','2021-02-13','LK Oberbergischer Kreis');
+module.exports = getNewZombies;
