1f79faedd5350e2288a1f1556e7c970a876bd869
Benjamin Salamon <benjamin.salamon@smail.th-koeln.de>
2021-02-15 09:49:38 +0100

    getDate Function hinzugefügt. ZombiesInNumbers.js weiter verbessert.

--- proof_of_concept/modules/ZombiesInNumbers.js
+++ proof_of_concept/modules/ZombiesInNumbers.js
@@ -1,19 +1,18 @@
 // Abfrage ans RKI zu den Infektionszahlen pro Tag pro Landkreis oder Stadt zum Überprüfen der Prognoserechnung
 
 const getNewZombies = require('./getNewZombies');
-
+const getDate = require('./getDate');
 //for ( let i=1; i<=9; i++) {
 //    getNewZombies(1, '2021-01-0'+i, '2021-01-0'+i, '12071', (intValue) => {console.log(intValue)});
 //}
 
 (async function() {
-    for(let i = 1; i <=14; i++){
+    for(let i = -7; i <0; i++){
         await new Promise(next => {
-            getNewZombies(1, '2021-02-'+i, '2021-02-'+i, '12071', (intValue) =>{
-                console.log(i+' '+intValue);
+            getNewZombies(1, getDate(i), getDate(i), '12071', (intValue) =>{
+                console.log(getDate(i)+' '+intValue);
                 next();
             });
         })
     }
 })()
-
---
+++ proof_of_concept/modules/getDate.js
@@ -0,0 +1,15 @@
+//Gibt ein String eines Datums aus, welches entweder +x Tage in der Zukunft liegt oder -x Tage in der Vergangenheit
+function getDate(tage){
+    let MyDate = new Date();
+    let MyDateString;
+
+    MyDate.setDate(MyDate.getDate()+tage);
+
+    MyDateString = (
+        (MyDate.getFullYear() + '-')
+        + ('0' + (MyDate.getMonth()+1)).slice(-2) + '-'
+        + ('0' + MyDate.getDate()).slice(-2))
+    ;
+    return(MyDateString);
+}
+module.exports = getDate;
