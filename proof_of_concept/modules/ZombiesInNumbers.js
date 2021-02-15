e7e1c81fa4db9597acf33d2860ebdc66b30e3b4a
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2021-02-15 14:48:38 +0100

    Testcases der Prognose Rechnung mit Beispielen zur Ermittlung der Abweichung.

--- proof_of_concept/modules/ZombiesInNumbers.js
+++ proof_of_concept/modules/ZombiesInNumbers.js
@@ -5,11 +5,12 @@ const getDate = require('./getDate');
 //for ( let i=1; i<=9; i++) {
 //    getNewZombies(1, '2021-01-0'+i, '2021-01-0'+i, '12071', (intValue) => {console.log(intValue)});
 //}
+// OBK=05374, Köln=05315, Dresden=14612
 
 (async function() {
-    for(let i = -7; i <0; i++){
+    for(let i = -350; i <-308; i++){
         await new Promise(next => {
-            getNewZombies(1, getDate(i), getDate(i), '12071', (intValue) =>{
+            getNewZombies(1, getDate(i), getDate(i), '13003', (intValue) =>{
                 console.log(getDate(i)+' '+intValue);
                 next();
             });
