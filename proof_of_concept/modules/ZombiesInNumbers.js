02626809514860fe14906fdd1b8c88acd76242b0
Thomas Schumann <thomas.schumann@smail.th-koeln.de>
2021-02-15 01:18:29 +0100

    ZombiesInNumbers again

--- proof_of_concept/modules/ZombiesInNumbers.js
+++ proof_of_concept/modules/ZombiesInNumbers.js
@@ -1,8 +1,20 @@
 // Abfrage ans RKI zu den Infektionszahlen pro Tag pro Landkreis oder Stadt zum Überprüfen der Prognoserechnung
 
-const getNewZombies = require('links');
+const getNewZombies = require('./getNewZombies');
 
-for ( let i=1; i<=9; i++) {
-    console.log(i)
-        getNewZombies(1, '2021-01-0'+i, '2021-01-0'+i, 'SK Köln', (intValue) => {console.log(intValue)});
-}
+//for ( let i=1; i<=9; i++) {
+//    getNewZombies(1, '2021-01-0'+i, '2021-01-0'+i, '12071', (intValue) => {console.log(intValue)});
+//}
+
+
+
+const forLoop = async _ => {
+    for ( let i=1; i<=14; i++) {
+        setTimeout(function() {
+            getNewZombies(1, '2021-02-'+i, '2021-02-'+i, '12071', (intValue) =>{console.log(i+' '+intValue)});}, 5000);
+        }
+
+        //   let newZombie = getNewZombies(1, '2021-01-0'+i, '2021-01-0'+i, '12071', (intValue) => {console.log(intValue)});
+    }
+
+forLoop();
