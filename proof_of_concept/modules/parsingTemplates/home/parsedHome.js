191159c19e78e61408797b2890d137a4e4f906c8
Benjamin Salamon <benjamin.salamon@smail.th-koeln.de>
2021-02-16 14:45:29 +0100

    Verbesserung Home

--- proof_of_concept/modules/parsingTemplates/home/parsedHome.js
+++ proof_of_concept/modules/parsingTemplates/home/parsedHome.js
@@ -1,16 +1,18 @@
 const ST = require('stjs');
 
-function parsedHome(array, callback){
-    const parsed = ST.select({"items": array})
-        .transformWith({
-            "{{#each items}}": {
-                "Titel": "{{this.titel}}",
-                "_links": {
-                    "self": {"href": "/{{this.link}}"}
+function parsedHome(array){
+    return new Promise((resolve, reject)=> {
+        const parsed = ST.select({"items": array})
+            .transformWith({
+                "{{#each items}}": {
+                    "Titel": "{{this.titel}}",
+                    "_links": {
+                        "self": {"href": "/{{this.link}}"}
+                    }
                 }
-            }
-        })
-        .root();
-    callback(parsed);
+            })
+            .root();
+        resolve(parsed);
+    })
 }
 module.exports = parsedHome;
